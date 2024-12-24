60daad4d089356c80c7ff81a9b470228540f12eb
shivank-1 <devliyal91@gmail.com>
2024-12-25 00:51:00 +0530

    First Commit

---
+++ agents.py
@@ -0,0 +1,43 @@
+from crewai import Agent
+from tools import yt_tool
+from langchain_groq import ChatGroq
+
+
+import os
+
+llm = ChatGroq(api_key="",temperature=0, model_name="llama3-70b-8192")
+
+
+## Create a senior blog content researcher
+
+blog_researcher=Agent(
+    role='Blog Researcher from Youtube Videos',
+    goal='get the relevant video transcription for the topic {topic} from the provided Yt channel',
+    verbose=True,
+    memory=True,
+    backstory=(
+       "Expert in understanding videos in AI Data Science , MAchine Learning And GEN AI and providing suggestion" 
+    ),
+    tools=[yt_tool],
+    allow_delegation=True,
+    llm=llm
+)
+
+## creating a senior blog writer agent with YT tool
+
+blog_writer=Agent(
+    role='Blog Writer',
+    goal='Narrate compelling tech stories about the video {topic} from YT video',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "With a flair for simplifying complex topics, you craft"
+        "engaging narratives that captivate and educate, bringing new"
+        "discoveries to light in an accessible manner."
+    ),
+    tools=[yt_tool],
+    allow_delegation=False,
+    llm=llm
+
+
+)
